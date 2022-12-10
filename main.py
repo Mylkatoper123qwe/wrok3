4fd176a7921d28ef035dfd7078edf13ee96a3b04
nshs33-102 <ualixzzy@gmail.com>
2022-12-10 14:37:44 +0200

    Load

--- main.py
+++ main.py
@@ -165,6 +165,84 @@ def shopping(self, manage):
             self.gladness+=10
             self.satiety+=2
             self.money-=15
+#Metod Chill
+def chill(self):
+    self.gladness+=10
+    self.home.mess+=5
+
+def clean_home(self):
+    self.gladness-=5
+    self.home.mess = 0
+
+def to_repair(self):
+    self.car.strenght+=100
+    self.money-=50
+#Parametr vivod
+def days_indexes(self,day):
+    day = f"Today the {day} of {self.name}'s life"
+    print(f"{day:=50}","\n")
+    human_indexes = self.name + "'s indexes"
+    print(f"{human_indexes:50}","\n")
+    print(f"Money - {self.money}")
+    print(f"Satiety - {self.satiety}")
+    print(f"Gladness - {self.gladness}")
+    home_indexes = "Home indexes"
+    print(f"{home_indexes:50}","\n")
+    print(f"Food - {self.home.food}")
+
+#Workout Classes
+print(f"Mess - {self.home.mess}")
+car_indexes = f"{self.car.brand} car indexes"
+print(f"{car_indexes:50}", "\n")
+print(f"Fuel - {self.car.fuel}")
+print(f"Strenght - {self.car.strenght}")
+
+#Def alive
+def is_alive(self):
+    if self.gladness<0:
+        print("Depression...")
+        return False
+    if self.satiety<0:
+        print("Dead...")
+        return False
+    if self.money<-500:
+        print("Bankrupt...")
+        return False
+
+#Simyliation
+def live (self,day):
+    if self.is_alive() == False:
+        return False
+    if self.home is None:
+        print("Settled in the house")
+        self.get_home()
+    if self.car is None:
+        self.get_car()
+        print(f"I bought a car {self.car.brand}")
+    if self.job is None:
+        self.get_job()
+        print(f"I don't have a job,I'm going to get  a job {self.job.job} with salary {self.job.salary}")
+        self.days_indexes(day)
+
+#Zanatia day
+dice = random.randint(1,4)
+if self.satiety<20:
+    print("I'll go eat")
+    self.eat()
+elif self.gladness < 20:
+    if self.home.mess>15:
+        print("I want to chill , but there is  so much mess..\nSo I will clean to the house")
+        self.clean_home()
+    else:
+        print("Let's chill!")
+        self.chill()
+elif self.money<0:
+    print("Start working ")
+    self.work()
+elif self.car.strenght<15:
+    print("I need to repair my car")
+    self.to_repair()
+
 
 
 
