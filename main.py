f0bed8f6afad8597870c220f8d67ffb70bde9cd2
nshs33-102 <ualixzzy@gmail.com>
2022-12-10 14:06:15 +0200

    Дальше

--- main.py
+++ main.py
@@ -88,7 +88,86 @@ class House:
 class Job:
     def __init__(self,job_list):
         self.job=random.choice(list(job_list))
-        self.salary=job_list
+        self.salary=job_list[self.job]["salary"]
+        self.gladnees_less=job_list[self.job]["Gladness_less"]
+
+#Job_list
+job_list = {
+    "Java developer":
+    {"Salary":50, "gladness_less": 10 },
+    "Python developer":
+    {"salary":40,"gladness_less": 3 },
+    "C++ developer":
+    {"salary":45,"gladness_less": 25},
+    "Rust Developer":
+    {"salary":70,"gladness_less": 1}
+}
+
+#Metod pull house
+def get_home(self):
+    self.home = House()
+def get_car(self):
+    self.car = Auto(brands_of_car)
+def get_job(self):
+    if self.car.drive():
+        pass
+    else:
+        self.to_repair()
+        return
+    self.job = Job(job_list)
+
+#Eat
+def eat(self):
+    if self.home.food <=0
+        self.shopping("food")
+    else:
+        if self.satiety >=100
+            self.satiety = 100
+            return
+        self.satiety+=5
+        self.home.food-=5
+
+#Def work
+def work(self):
+    if self.car.drive():
+        pass
+    else:
+        if self.car.fuel < 20:
+            self.shopping("fuel")
+            return
+        else:
+            self.to_repair()
+            return
+        self.money += self.job.salary
+        self.gladness -= self.job.gladnees_less
+        self.satiety -=4
+
+#Metod Byu
+def shopping(self, manage):
+    if self.car.drive():
+        pass
+    else:
+        if self.car.fuel < 20:
+            manage = "fuel"
+        else:
+            self.to.repair()
+            return
+        if manage == "fuel":
+            print("I bought fuel")
+            self.money -=100
+            self.car.fuel +=100
+        elif manage == "food":
+            print("Bought food")
+            self.money -=50
+            self.home.food +=50
+        elif manage == "delicacies":
+            print("Hooray! Delicious!")
+            self.gladness+=10
+            self.satiety+=2
+            self.money-=15
+
+
+
 
 
 
